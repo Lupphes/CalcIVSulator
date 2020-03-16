3edea7f78718612ad8cf4467545d408241804ed3
Viktor Rucký <1508117+alpatron@users.noreply.github.com>
2020-03-16 21:29:50 +0100

    Creating scaffolding functions in the library

--- src/IVSSemesterProject/IVSMathLibrary/IVSMath.cs
+++ src/IVSSemesterProject/IVSMathLibrary/IVSMath.cs
@@ -8,6 +8,59 @@ namespace IVSMathLibrary
 {
     public class IVSMath
     {
-        
+        public static double Add(double augend, double addend)
+        {
+            return 0;
+        }
+
+        public static double Substract(double minuend, double subtrahend)
+        {
+            return 0;
+        }
+
+        public static double Multiply(double multiplier, double multiplicand)
+        {
+            return 0;
+        }
+
+        public static double Divide(double dividend, double divisor)
+        {
+            return 0;
+        }
+
+        public static double Root(double radicand, double degree)
+        {
+            return 0;
+        }
+
+        public static double Power(double base_, int exponent)
+        {
+            return 0;
+        }
+
+        public static int Factorial(int a)
+        {
+            return 0;
+        }
+
+        public static double Inverse(double a)
+        {
+            return 0;
+        }
+
+        public static double Sine(double a)
+        {
+            return 0;
+        }
+
+        public static double Cosine(double a)
+        {
+            return 0;
+        }
+
+        public static double Tangent(double a)
+        {
+            return 0;
+        }
     }
 }
