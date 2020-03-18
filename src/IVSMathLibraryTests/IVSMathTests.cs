1bef2a9f59df6fe8ff5789fc4e1c47d6d8eee0bd
Ondřej Sloup <xsloup02@vutbr.cz>
2020-03-18 23:41:21 +0100

    Unit Test Solution initialized

---
+++ src/IVSMathLibraryTests/IVSMathTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using IVSMathLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IVSMathLibraryTests
+{
+    [TestClass]
+    public class IVSMathTests
+    {
+        [TestMethod]
+        public void Add_Calculation_ReturnsEqual()
+        {
+            // Arrange 
+            double result;
+            double expected = 10;
+
+            // Act
+            result = IVSMath.Add(5, 5);
+
+            // Assert
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod]
+        public void Substract_Calculation_ReturnsTrue()
+        {
+        }
+
+        [TestMethod]
+        public void Multiply_Calculation_ReturnsTrue()
+        {
+        }
+
+        [TestMethod]
+        public void Divide_Calculation_ReturnsTrue()
+        {
+        }
+
+        [TestMethod]
+        public void Root_Calculation_ReturnsTrue()
+        {
+        }
+        [TestMethod]
+        public void Power_Calculation_ReturnsTrue()
+        {
+        }
+
+        [TestMethod]
+        public void Inverse_Calculation_ReturnsTrue()
+        {
+        }
+
+        [TestMethod]
+        public void Sine_Calculation_ReturnsTrue()
+        {
+        }
+
+        [TestMethod]
+        public void Cosine_Calculation_ReturnsTrue()
+        {
+        }
+
+        [TestMethod]
+        public void Tangent_Calculation_ReturnsTrue()
+        {
+        }
+    }
+}
---
+++ src/IVSMathLibraryTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("IVSMathLibraryTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("IVSMathLibraryTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ca2639b3-0115-4dcf-8a30-44d138f432d4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
