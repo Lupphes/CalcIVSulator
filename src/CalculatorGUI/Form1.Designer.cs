c51430833955db23c6af20f4c67bcd9576469f51
Ondřej Sloup <xsloup02@vutbr.cz>
2020-03-22 16:20:57 +0100

    Added basic functions

--- src/CalculatorGUI/Form1.Designer.cs
+++ src/CalculatorGUI/Form1.Designer.cs
@@ -46,11 +46,12 @@
             this.btn_PowTo1 = new System.Windows.Forms.Button();
             this.btn_Root = new System.Windows.Forms.Button();
             this.btn_Power = new System.Windows.Forms.Button();
-            this.btn_comma = new System.Windows.Forms.Button();
+            this.btn_Comma = new System.Windows.Forms.Button();
             this.btn_sin = new System.Windows.Forms.Button();
             this.btn_cos = new System.Windows.Forms.Button();
             this.btn_tan = new System.Windows.Forms.Button();
             this.rtb_Out = new System.Windows.Forms.RichTextBox();
+            this.btn_Back = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btn_Nbr_3
@@ -307,19 +308,19 @@
             this.btn_Power.UseVisualStyleBackColor = false;
             this.btn_Power.Click += new System.EventHandler(this.btn_Power_Click);
             // 
-            // btn_comma
+            // btn_Comma
             // 
-            this.btn_comma.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
-            this.btn_comma.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.btn_comma.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_comma.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
-            this.btn_comma.Location = new System.Drawing.Point(110, 269);
-            this.btn_comma.Name = "btn_comma";
-            this.btn_comma.Size = new System.Drawing.Size(47, 36);
-            this.btn_comma.TabIndex = 28;
-            this.btn_comma.Text = ",";
-            this.btn_comma.UseVisualStyleBackColor = false;
-            this.btn_comma.Click += new System.EventHandler(this.btn_comma_Click);
+            this.btn_Comma.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.btn_Comma.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_Comma.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Comma.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.btn_Comma.Location = new System.Drawing.Point(110, 269);
+            this.btn_Comma.Name = "btn_Comma";
+            this.btn_Comma.Size = new System.Drawing.Size(47, 36);
+            this.btn_Comma.TabIndex = 28;
+            this.btn_Comma.Text = ",";
+            this.btn_Comma.UseVisualStyleBackColor = false;
+            this.btn_Comma.Click += new System.EventHandler(this.btn_Comma_Click);
             // 
             // btn_sin
             // 
@@ -368,16 +369,28 @@
             this.rtb_Out.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.rtb_Out.Font = new System.Drawing.Font("Microsoft PhagsPa", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.rtb_Out.Location = new System.Drawing.Point(4, 5);
-            this.rtb_Out.MaxLength = 9;
+            this.rtb_Out.MaxLength = 10;
             this.rtb_Out.Multiline = false;
             this.rtb_Out.Name = "rtb_Out";
             this.rtb_Out.ReadOnly = true;
-            this.rtb_Out.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
             this.rtb_Out.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
             this.rtb_Out.Size = new System.Drawing.Size(259, 90);
             this.rtb_Out.TabIndex = 32;
-            this.rtb_Out.Text = "0";
-            this.rtb_Out.ZoomFactor = 2F;
+            this.rtb_Out.Text = "";
+            // 
+            // btn_Back
+            // 
+            this.btn_Back.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.btn_Back.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_Back.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Back.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.btn_Back.Location = new System.Drawing.Point(216, 143);
+            this.btn_Back.Name = "btn_Back";
+            this.btn_Back.Size = new System.Drawing.Size(47, 36);
+            this.btn_Back.TabIndex = 33;
+            this.btn_Back.Text = "←";
+            this.btn_Back.UseVisualStyleBackColor = false;
+            this.btn_Back.Click += new System.EventHandler(this.btn_Back_Click);
             // 
             // CalcForm
             // 
@@ -385,11 +398,12 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.AliceBlue;
             this.ClientSize = new System.Drawing.Size(265, 307);
+            this.Controls.Add(this.btn_Back);
             this.Controls.Add(this.rtb_Out);
             this.Controls.Add(this.btn_tan);
             this.Controls.Add(this.btn_cos);
             this.Controls.Add(this.btn_sin);
-            this.Controls.Add(this.btn_comma);
+            this.Controls.Add(this.btn_Comma);
             this.Controls.Add(this.btn_Power);
             this.Controls.Add(this.btn_Root);
             this.Controls.Add(this.btn_PowTo1);
@@ -437,11 +451,12 @@
         private System.Windows.Forms.Button btn_PowTo1;
         private System.Windows.Forms.Button btn_Root;
         private System.Windows.Forms.Button btn_Power;
-        private System.Windows.Forms.Button btn_comma;
+        private System.Windows.Forms.Button btn_Comma;
         private System.Windows.Forms.Button btn_sin;
         private System.Windows.Forms.Button btn_cos;
         private System.Windows.Forms.Button btn_tan;
         private System.Windows.Forms.RichTextBox rtb_Out;
+        private System.Windows.Forms.Button btn_Back;
     }
 }
 
--- src/CalculatorGUI/Form1.cs
+++ src/CalculatorGUI/Form1.cs
@@ -20,115 +20,118 @@ namespace CalculatorGUI
 
         private void btn_Nbr_0_Click(object sender, EventArgs e)
         {
-
+            rtb_Out.Text += "0";
         }
 
         private void btn_Nbr_1_Click(object sender, EventArgs e)
         {
-
+            rtb_Out.Text += "1";
         }
 
         private void btn_Nbr_2_Click(object sender, EventArgs e)
         {
-
+            rtb_Out.Text += "2";
         }
 
         private void btn_Nbr_3_Click(object sender, EventArgs e)
         {
-
+            rtb_Out.Text += "3";
         }
 
         private void btn_Nbr_4_Click(object sender, EventArgs e)
         {
-
+            rtb_Out.Text += "4";
         }
 
         private void btn_Nbr_5_Click(object sender, EventArgs e)
         {
-
+            rtb_Out.Text += "5";
         }
 
         private void btn_Nbr_6_Click(object sender, EventArgs e)
         {
-
+            rtb_Out.Text += "6";
         }
 
         private void btn_Nbr_7_Click(object sender, EventArgs e)
         {
-
+            rtb_Out.Text += "7";
         }
 
         private void btn_Nbr_8_Click(object sender, EventArgs e)
         {
-
+            rtb_Out.Text += "8";
         }
 
         private void btn_Nbr_9_Click(object sender, EventArgs e)
         {
-
+            rtb_Out.Text += "9";
         }
 
-        private void btn_comma_Click(object sender, EventArgs e)
+        private void btn_Comma_Click(object sender, EventArgs e)
         {
-
+            rtb_Out.Text += ",";
         }
-
+        private void btn_Back_Click(object sender, EventArgs e)
+        {
+            rtb_Out.Text = "";
+        }
 
         // -----------------------------------------------------
 
         private void btn_Add_Click(object sender, EventArgs e)
         {
-
+            rtb_Out.Text += "+";
         }
 
         private void btn_Sub_Click(object sender, EventArgs e)
         {
-
+            rtb_Out.Text += "-";
         }
 
         private void btn_Mul_Click(object sender, EventArgs e)
         {
-
+            rtb_Out.Text += "*";
         }
 
         private void btn_Div_Click(object sender, EventArgs e)
         {
-
+            rtb_Out.Text += "/";
         }
 
         private void btn_Root_Click(object sender, EventArgs e)
         {
-
+            rtb_Out.Text += "sqrt(";
         }
 
         private void btn_Power_Click(object sender, EventArgs e)
         {
-
+            rtb_Out.Text += "^";
         }
 
         private void btn_PowTo1_Click(object sender, EventArgs e)
         {
-
-        }
-
-        private void btn_Res_Click(object sender, EventArgs e)
-        {
-
+            rtb_Out.Text += "^-1";
         }
 
         private void btn_sin_Click(object sender, EventArgs e)
         {
-
+            rtb_Out.Text += "sin(";
         }
 
         private void btn_cos_Click(object sender, EventArgs e)
         {
-
+            rtb_Out.Text += "cos(";
         }
 
         private void btn_tan_Click(object sender, EventArgs e)
         {
-
+            rtb_Out.Text += "tan(";
+        }
+        private void btn_Res_Click(object sender, EventArgs e)
+        {
+            string input = rtb_Out.Text;
+            rtb_Out.Text = "Calculating...";
         }
 
     }
