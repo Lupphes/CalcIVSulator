9862b858cca264a82de96dc3aa80c54d6ea5a89a
Ondřej Sloup <xsloup02@vutbr.cz>
2020-03-25 01:28:14 +0100

    Fixed some values, Added precision, Splited into multiple tests

--- src/IVSMathLibraryTests/IVSMathTests.cs
+++ src/IVSMathLibraryTests/IVSMathTests.cs
@@ -12,118 +12,170 @@ namespace IVSMathLibraryTests
 
         [TestMethod]
         [ExpectedException(typeof(OverflowException))]
-        public void NumberValidation_Overflow_ReturnExeptions()
+        public void NumberValidation_OverflowAdd_ReturnExeptions()
         {
-            // Add
             IVSMath.Add(double.MaxValue, 1);
             IVSMath.Add(double.MinValue, -1);
+        }
 
-            // Substract
+        public void NumberValidation_OverflowSubstract_ReturnExeptions()
+        {
             IVSMath.Substract(double.MaxValue, -5);
             IVSMath.Substract(double.MinValue, 5);
+        }
 
-            // Multiply
+        public void NumberValidation_OverflowMultiply_ReturnExeptions()
+        {
             IVSMath.Multiply(double.MaxValue, 2);
             IVSMath.Multiply(double.MinValue, 0.5);
+        }
 
-            // Divide
+        public void NumberValidation_OverflowDivide_ReturnExeptions()
+        { 
             IVSMath.Divide(double.MaxValue, 0.5);
-            IVSMath.Divide(double.MinValue, 0.5);
-
+            IVSMath.Divide(double.MinValue, 5);
         }
 
         [TestMethod]
-        public void Add_Calculation_ReturnEqual()
+        public void Add_CalculationZero_ReturnEqual()
         {
+            Assert.AreEqual(0, IVSMath.Add(0, 0), 0);
+        }
 
-            // Zero
-            Assert.AreEqual(0, IVSMath.Add(0, 0));
+        [TestMethod]
+        public void Add_CalculationWholeNumbers_ReturnEqual()
+        {
+            Assert.AreEqual(10, IVSMath.Add(5, 5), 0);
+            Assert.AreEqual(5, IVSMath.Add(0, 5), 0);
+            Assert.AreEqual(5, IVSMath.Add(5, 0), 0);
+        }
 
-            // Positive whole numbers
-            Assert.AreEqual(10, IVSMath.Add(5, 5));
-            Assert.AreEqual(5, IVSMath.Add(0, 5));
-            Assert.AreEqual(5, IVSMath.Add(5, 0));
+        [TestMethod]
+        public void Add_CalculationNegativeWholeNumbers_ReturnEqual()
+        {
+            Assert.AreEqual(-10, IVSMath.Add(-5, -5), 0);
+            Assert.AreEqual(-5, IVSMath.Add(0, -5), 0);
+            Assert.AreEqual(-5, IVSMath.Add(-5, 0), 0);
+        }
 
-            // Negative whole numbers
-            Assert.AreEqual(-10, IVSMath.Add(-5, -5));
-            Assert.AreEqual(-5, IVSMath.Add(0, -5));
-            Assert.AreEqual(-5, IVSMath.Add(-5, 0));
+        [TestMethod]
+        public void Add_CalculationDecimalNumbers_ReturnEqual()
+        {
+            Assert.AreEqual(11.1, IVSMath.Add(5.5, 5.6), 0);
+            Assert.AreEqual(5.4, IVSMath.Add(0, 5.4), 0);
+            Assert.AreEqual(5.6, IVSMath.Add(5.6, 0), 0);
+        }
 
-            // Positive decimal numbers
-            Assert.AreEqual(11.1, IVSMath.Add(5.5, 5.6));
-            Assert.AreEqual(5.4, IVSMath.Add(0, 5.4));
-            Assert.AreEqual(5.6, IVSMath.Add(5.6, 0));
+        [TestMethod]
+        public void Add_CalculationNegativeDecimalNumbers_ReturnEqual()
+        {
+            Assert.AreEqual(-11, IVSMath.Add(-5.9, -5.1), 0);
+            Assert.AreEqual(-5.9, IVSMath.Add(0, -5.9), 0);
+            Assert.AreEqual(-5.2, IVSMath.Add(-5.2, 0), 0);
+        }
 
-            // Negative decimal numbers
-            Assert.AreEqual(-11, IVSMath.Add(-5.9, -5.1));
-            Assert.AreEqual(-5.9, IVSMath.Add(0, -5.9));
-            Assert.AreEqual(-5.2, IVSMath.Add(-5.2, 0));
-
-            // Positive whole rational numbers
+        [TestMethod]
+        public void Add_CalculationRationalNumbers_ReturnEqual()
+        {
             Assert.AreEqual(2 * Math.PI, IVSMath.Add(Math.PI, Math.PI), precision);
             Assert.AreEqual(Math.PI, IVSMath.Add(Math.PI, -Math.PI), precision);
 
         }
 
         [TestMethod]
-        public void Substract_Calculation_ReturnTrue()
+        public void Substract_CalculationZero_ReturnEqual()
         {
-            // Zero
-            Assert.AreEqual(0, IVSMath.Add(0, 0));
-
-            // Positive whole numbers
-            Assert.AreEqual(0, IVSMath.Substract(5, 5));
-            Assert.AreEqual(-5, IVSMath.Substract(0, 5));
-            Assert.AreEqual(-5, IVSMath.Substract(5, 0));
-
-            // Negative whole numbers
-            Assert.AreEqual(0, IVSMath.Substract(-5, -5));
-            Assert.AreEqual(10, IVSMath.Substract(5, -5));
-            Assert.AreEqual(-5, IVSMath.Substract(0, -5));
-            Assert.AreEqual(-5, IVSMath.Substract(-5, 0));
-
-            // Positive decimal numbers
-            Assert.AreEqual(11.1, IVSMath.Substract(5.5, 5.6));
-            Assert.AreEqual(5.4, IVSMath.Substract(0, 5.4));
-            Assert.AreEqual(5.6, IVSMath.Substract(5.6, 0));
-
-            // Negative decimal numbers
-            Assert.AreEqual(-11, IVSMath.Substract(-5.9, -5.1));
-            Assert.AreEqual(-5.9, IVSMath.Substract(0, -5.9));
-            Assert.AreEqual(-5.2, IVSMath.Substract(-5.2, 0));
-
-            // Positive whole rational numbers
-            Assert.AreEqual(0, IVSMath.Substract(Math.PI, Math.PI));
-            Assert.AreEqual(Math.PI, IVSMath.Substract(Math.PI, -Math.PI), precision);
+            Assert.AreEqual(0, IVSMath.Add(0, 0), 0);
         }
 
         [TestMethod]
-        public void Multiply_Calculation_ReturnTrue()
+        public void Substract_CalculationWholeNumbers_ReturnEqual()
         {
-            Assert.AreEqual(25, IVSMath.Multiply(5, 5));
-            Assert.AreEqual(0, IVSMath.Multiply(0, 5));
-            Assert.AreEqual(0, IVSMath.Multiply(5, 0));
+            Assert.AreEqual(0, IVSMath.Substract(5, 5), 0);
+            Assert.AreEqual(-5, IVSMath.Substract(0, 5), 0);
+            Assert.AreEqual(-5, IVSMath.Substract(5, 0), 0);
+        }
 
-            // Negative whole numbers
-            Assert.AreEqual(25, IVSMath.Multiply(-5, -5));
-            Assert.AreEqual(-25, IVSMath.Multiply(5, -5));
-            Assert.AreEqual(-25, IVSMath.Multiply(-5, 5));
-            Assert.AreEqual(0, IVSMath.Multiply(0, -5));
-            Assert.AreEqual(0, IVSMath.Multiply(-5, 0));
+        [TestMethod]
+        public void Substract_CalculationNegativeWholeNumbers_ReturnEqual() {
+            Assert.AreEqual(0, IVSMath.Substract(-5, -5), 0);
+            Assert.AreEqual(10, IVSMath.Substract(5, -5), 0);
+            Assert.AreEqual(5, IVSMath.Substract(0, -5), 0);
+            Assert.AreEqual(-5, IVSMath.Substract(-5, 0), 0);
+        }
 
-            // Positive decimal numbers
-            Assert.AreEqual(30.8, IVSMath.Multiply(5.5, 5.6));
-            Assert.AreEqual(0, IVSMath.Multiply(0, 5.4));
-            Assert.AreEqual(0, IVSMath.Multiply(5.6, 0));
+        [TestMethod]
+        public void Substract_CalculationDecimalNumbers_ReturnEqual()
+        {
+            Assert.AreEqual(-0.1, IVSMath.Substract(5.5, 5.6), 0);
+            Assert.AreEqual(-5.4, IVSMath.Substract(0, 5.4), 0);
+            Assert.AreEqual(5.6, IVSMath.Substract(5.6, 0), 0);
+        }
 
-            // Negative decimal numbers
-            Assert.AreEqual(30.09, IVSMath.Multiply(-5.9, -5.1));
-            Assert.AreEqual(0, IVSMath.Multiply(0, -5.9));
-            Assert.AreEqual(0, IVSMath.Multiply(-5.2, 0));
+        [TestMethod]
+        public void Substract_CalculationNegativeDecimalNumbers_ReturnEqual()
+        {
+            Assert.AreEqual(-0.8, IVSMath.Substract(-5.9, -5.1));
+            Assert.AreEqual(5.9, IVSMath.Substract(0, -5.9));
+            Assert.AreEqual(-5.2, IVSMath.Substract(-5.2, 0));
+        }
 
-            // Positive whole rational numbers
-            Assert.AreEqual(2 * Math.PI, IVSMath.Multiply(Math.PI, Math.PI), precision);
-            Assert.AreEqual(-2 * Math.PI, IVSMath.Multiply(Math.PI, -Math.PI), precision);
+        [TestMethod]
+        public void Substract_CalculationRationalNumbers_ReturnEqual()
+        {
+            Assert.AreEqual(0, IVSMath.Substract(Math.PI, Math.PI), 0);
+            Assert.AreEqual(2 * Math.PI, IVSMath.Substract(Math.PI, -Math.PI), precision);
+            Assert.AreEqual(-2 * Math.PI, IVSMath.Substract(-Math.PI, Math.PI), precision);
+            Assert.AreEqual(0, IVSMath.Substract(-Math.PI, -Math.PI), 0);
+        }
+
+        [TestMethod]
+        public void Multiply_CalculationZero_ReturnTrue()
+        {
+            Assert.AreEqual(0, IVSMath.Multiply(0, 0), 0);
+        }
+
+        [TestMethod]
+        public void Multiply_CalculationWholeNumbers_ReturnTrue()
+        {
+            Assert.AreEqual(25, IVSMath.Multiply(5, 5), 0);
+            Assert.AreEqual(0, IVSMath.Multiply(0, 5), 0);
+            Assert.AreEqual(0, IVSMath.Multiply(5, 0), 0);
+        }
+
+        [TestMethod]
+        public void Multiply_CalculationNegativeWholeNumbers_ReturnTrue()
+        {
+            Assert.AreEqual(25, IVSMath.Multiply(-5, -5), 0);
+            Assert.AreEqual(-25, IVSMath.Multiply(5, -5), 0);
+            Assert.AreEqual(-25, IVSMath.Multiply(-5, 5), 0);
+            Assert.AreEqual(0, IVSMath.Multiply(0, -5), 0);
+            Assert.AreEqual(0, IVSMath.Multiply(-5, 0), 0);
+        }
+
+        [TestMethod]
+        public void Multiply_CalculationDecimalNumbers_ReturnTrue()
+        {
+            Assert.AreEqual(30.8, IVSMath.Multiply(5.5, 5.6), 0);
+            Assert.AreEqual(0, IVSMath.Multiply(0, 5.4), 0);
+            Assert.AreEqual(0, IVSMath.Multiply(5.6, 0), 0);
+        }
+
+        [TestMethod]
+        public void Multiply_CalculationNegativeDecimalNumbers_ReturnTrue()
+        {
+            Assert.AreEqual(30.09, IVSMath.Multiply(-5.9, -5.1), 0);
+            Assert.AreEqual(0, IVSMath.Multiply(0, -5.9), 0);
+            Assert.AreEqual(0, IVSMath.Multiply(-5.2, 0), 0);
+        }
+
+        [TestMethod]
+        public void Multiply_CalculationRationalNumbers_ReturnTrue()
+        {
+            Assert.AreEqual(Math.PI * Math.PI, IVSMath.Multiply(Math.PI, Math.PI), precision);
+            Assert.AreEqual(Math.PI * -Math.PI, IVSMath.Multiply(Math.PI, -Math.PI), precision);
+            Assert.AreEqual(-Math.PI * -Math.PI, IVSMath.Multiply(-Math.PI, Math.PI), precision);
+            Assert.AreEqual(-Math.PI * -Math.PI, IVSMath.Multiply(-Math.PI, -Math.PI), precision);
         }
 
         [TestMethod]
@@ -134,27 +186,41 @@ namespace IVSMathLibraryTests
         }
 
         [TestMethod]
-        public void Divide_Calculation_ReturnTrue()
+        public void Divide_CalculationWholeNumbers_ReturnTrue()
         {
-            Assert.AreEqual(1, IVSMath.Divide(5, 5));
-            Assert.AreEqual(0, IVSMath.Divide(0, 5));
+            Assert.AreEqual(1, IVSMath.Divide(5, 5), 0);
+            Assert.AreEqual(0, IVSMath.Divide(0, 5), 0);
+        }
 
-            // Negative whole numbers
-            Assert.AreEqual(1, IVSMath.Divide(-5, -5));
-            Assert.AreEqual(-1, IVSMath.Divide(5, -5));
-            Assert.AreEqual(0, IVSMath.Divide(0, -5));
+        [TestMethod]
+        public void Divide_CalculationNegativeWholeNumbers_ReturnTrue()
+        {
+            Assert.AreEqual(1, IVSMath.Divide(-5, -5), 0);
+            Assert.AreEqual(-1, IVSMath.Divide(5, -5), 0);
+            Assert.AreEqual(0, IVSMath.Divide(0, -5), 0);
+        }
 
-            // Positive decimal numbers
-            Assert.AreEqual(2, IVSMath.Divide(8.6, 4.3));
-            Assert.AreEqual(0, IVSMath.Divide(0, 5.4));
+        [TestMethod]
+        public void Divide_CalculationDecimalNumbers_ReturnTrue()
+        {
+            Assert.AreEqual(2, IVSMath.Divide(8.6, 4.3), 0);
+            Assert.AreEqual(0, IVSMath.Divide(0, 5.4), 0);
+        }
 
-            // Negative decimal numbers
-            Assert.AreEqual(129, IVSMath.Divide(-25.8, -0.2));
-            Assert.AreEqual(0, IVSMath.Divide(0, -5.9));
+        [TestMethod]
+        public void Divide_CalculationNegativeDecimalNumbers_ReturnTrue()
+        {
+            Assert.AreEqual(129, IVSMath.Divide(-25.8, -0.2), 0);
+            Assert.AreEqual(0, IVSMath.Divide(0, -5.9), 0);
+        }
 
-            // Positive whole rational numbers
+        [TestMethod]
+        public void Divide_CalculationRationalNumbers_ReturnTrue()
+        {
             Assert.AreEqual(1, IVSMath.Divide(Math.PI, Math.PI));
             Assert.AreEqual(-1, IVSMath.Divide(Math.PI, -Math.PI));
+            Assert.AreEqual(-1, IVSMath.Divide(-Math.PI, Math.PI));
+            Assert.AreEqual(1, IVSMath.Divide(-Math.PI, -Math.PI));
         }
 
         [TestMethod]
