380b6af94e4c1f72c012afb8b892eab04a755bf0
Viktor Rucký <1508117+alpatron@users.noreply.github.com>
2020-03-25 16:00:10 +0100

    Implemented tests for the inverse function.

--- src/IVSMathLibraryTests/IVSMathTests.cs
+++ src/IVSMathLibraryTests/IVSMathTests.cs
@@ -54,10 +54,61 @@ namespace IVSMathLibraryTests
         }
 
         [TestMethod]
-        public void Inverse_Calculation_ReturnsTrue()
+        public void Inverse_CalculationOnePositive_ReturnsTrue()
         {
+            Assert.AreEqual(1, IVSMath.Inverse(1), 0);
         }
-
+        [TestMethod]
+        public void Inverse_CalculationOneNegative_ReturnsTrue()
+        {
+            Assert.AreEqual(-1, IVSMath.Inverse(-1), 0);
+        }
+        [TestMethod]
+        public void Inverse_CalculationZero_ReturnsExceptions()
+        {
+            Assert.ThrowsException<DivideByZeroException>(() => IVSMath.Inverse(0));
+        }
+        [TestMethod]
+        public void Inverse_CalculationIntegerPositive_ReturnsTrue()
+        {
+            Assert.AreEqual(1.0 / 5.0, IVSMath.Inverse(5), precision);
+            Assert.AreEqual(1.0 / 158963.0, IVSMath.Inverse(158963), precision);
+            Assert.AreEqual(1.0 / 42.0, IVSMath.Inverse(42), precision);
+        }
+        [TestMethod]
+        public void Inverse_CalculationRealPositiveAbsGreaterThanOne_ReturnsTrue()
+        {
+            Assert.AreEqual(8.0 / 41.0, IVSMath.Inverse(5.125), precision);
+            Assert.AreEqual(1.0 / 158963.45892, IVSMath.Inverse(158963.45892), precision);
+            Assert.AreEqual(50.0 / 2121.0, IVSMath.Inverse(42.42), precision);
+        }
+        [TestMethod]
+        public void Inverse_CalculationRealPositiveAbsLessThanOne_ReturnsTrue()
+        {
+            Assert.AreEqual(25, IVSMath.Inverse(0.04), precision);
+            Assert.AreEqual(5000.0 / 823.0, IVSMath.Inverse(0.1646), precision);
+            Assert.AreEqual(50.0 / 21.0, IVSMath.Inverse(0.42), precision);
+        }
+        [TestMethod]
+        public void Inverse_CalculationIntegerNegative_ReturnsTrue()
+        {
+            Assert.AreEqual(-(1.0 / 5.0), IVSMath.Inverse(-5), precision);
+            Assert.AreEqual(-(1.0 / 158963.0), IVSMath.Inverse(-158963), precision);
+            Assert.AreEqual(-(1.0 / 42.0), IVSMath.Inverse(-42), precision);
+        }
+        public void Inverse_CalculationRealNegativeAbsGreaterThanOne_ReturnsTrue()
+        {
+            Assert.AreEqual(-(8.0 / 41.0), IVSMath.Inverse(-5.125), precision);
+            Assert.AreEqual(-(1.0 / 158963.45892), IVSMath.Inverse(-158963.45892), precision);
+            Assert.AreEqual(-(50.0 / 2121.0), IVSMath.Inverse(-42.42), precision);
+        }
+        [TestMethod]
+        public void Inverse_CalculationRealNegativeAbsLessThanOne_ReturnsTrue()
+        {
+            Assert.AreEqual(-25, IVSMath.Inverse(-0.04), precision);
+            Assert.AreEqual(-(5000.0 / 823.0), IVSMath.Inverse(-0.1646), precision);
+            Assert.AreEqual(-(50.0 / 21.0), IVSMath.Inverse(-0.42), precision);
+        }
         [TestMethod]
         public void Sine_Calculation_ReturnsTrue()
         {
