70368b3fbe808f888abe022ed6d5dd34e9fc37d3
vlachvojta <vojtik.vlach@seznam.cz>
2020-04-12 20:02:20 +0200

    Math library linked

---
+++ src/SampleStandartDeviation/SSDeviation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IVSMathLibrary;
+
+namespace SampleStandartDeviation
+{
+    class SSDeviation
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            IVSMath.Sine(42);
+        }
+    }
+}
